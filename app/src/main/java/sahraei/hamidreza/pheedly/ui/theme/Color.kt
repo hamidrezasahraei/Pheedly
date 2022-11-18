ddf21fdb9fe3beffb97b391eb6256b80c37f93e8
Hamidreza <hamidrezasahraei@cafebazaar.ir>
2022-11-18 17:01:38 +0330

    Add libraries and theme staff

--- app/src/main/java/sahraei/hamidreza/pheedly/ui/theme/Color.kt
+++ app/src/main/java/sahraei/hamidreza/pheedly/ui/theme/Color.kt
@@ -2,10 +2,15 @@ package sahraei.hamidreza.pheedly.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val LightPheedly = Color(0xFFE9CE01)
+val LightGreys90 = Color(0xFF20222A)
+val LightGreys70 = Color(0xFF717485)
+val LightGreys40 = Color(0xFFE5E7F0)
+val LightGreys20 = Color(0xFFF9FAFD)
+val LightGreys10 = Color(0xFFFFFFFF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val DarkGreys90 = Color(0xFFFFFFFF)
+val DarkGreys70 = Color(0xFFC0C3D2)
+val DarkGreys40 = Color(0xFF44464E)
+val DarkGreys20 = Color(0xFF1D1F23)
+val DarkGreys10 = Color(0xFF191A1F)
