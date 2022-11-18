d7758669bdcf73e33e170201221e169a01eab309
Hamidreza <hamidrezasahraei@cafebazaar.ir>
2022-11-18 12:23:42 +0330

    Init project

---
+++ app/src/main/java/sahraei/hamidreza/pheedly/MainActivity.kt
@@ -0,0 +1,43 @@
+package sahraei.hamidreza.pheedly
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import sahraei.hamidreza.pheedly.ui.theme.PheedlyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PheedlyTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PheedlyTheme {
+        Greeting("Android")
+    }
+}
