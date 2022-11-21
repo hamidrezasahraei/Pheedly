f54ee96f8188abd10851c9929d7beb24dc6d3387
Hamidreza <hamidrezasahraei@cafebazaar.ir>
2022-11-21 16:04:56 +0330

    Add cardview for FeedListScreen.kt

---
+++ app/src/main/java/sahraei/hamidreza/pheedly/common/CardItem.kt
@@ -0,0 +1,36 @@
+package sahraei.hamidreza.pheedly.common
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import sahraei.hamidreza.pheedly.ui.theme.Typography
+
+@Composable
+fun CardItem(
+    modifier: Modifier = Modifier,
+    title: String,
+    onCardClick: (String) -> Unit
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .clickable {
+                onCardClick.invoke(title)
+            },
+        elevation = 8.dp
+    ) {
+        Text(
+            text = title,
+            style = Typography.body1,
+            color = MaterialTheme.colors.onBackground,
+            modifier = Modifier
+                .padding(16.dp)
+        )
+    }
+}
--- app/src/main/java/sahraei/hamidreza/pheedly/feature/feedlist/ui/FeedListScreen.kt
+++ app/src/main/java/sahraei/hamidreza/pheedly/feature/feedlist/ui/FeedListScreen.kt
@@ -1,17 +1,22 @@
 package sahraei.hamidreza.pheedly.feature.feedlist.ui
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Text
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import sahraei.hamidreza.pheedly.common.CardItem
 import sahraei.hamidreza.pheedly.common.PheedlyProgressItem
 import java.net.URLEncoder
 import java.nio.charset.StandardCharsets
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun FeedListScreen(
     feedsViewModel: FeedsViewModel = hiltViewModel(),
@@ -23,17 +28,21 @@ fun FeedListScreen(
             PheedlyProgressItem()
         }
         state.feeds != null -> {
-            LazyColumn() {
-                state.feeds.forEach {
-                    item {
-                        Text(
-                            text = it,
-                            modifier = Modifier
-                                .clickable {
-                                    val encodedUrl = URLEncoder.encode(it, StandardCharsets.UTF_8.toString())
-                                    onFeedClicked.invoke(encodedUrl)
-                                }
-                        )
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentPadding = PaddingValues(16.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                items(
+                    items = state.feeds,
+                    key = { it }
+                ) {
+                    CardItem(
+                        title = it
+                    ) { title ->
+                        val encodedUrl = URLEncoder.encode(title, StandardCharsets.UTF_8.toString())
+                        onFeedClicked.invoke(encodedUrl)
                     }
                 }
             }
