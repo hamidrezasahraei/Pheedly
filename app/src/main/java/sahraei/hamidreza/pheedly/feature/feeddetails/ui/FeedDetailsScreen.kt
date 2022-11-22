c71a4fd2566cc1e2f9279974177a6b5ca539a1eb
Hamidreza <hamidrezasahraei@cafebazaar.ir>
2022-11-22 15:36:26 +0330

    Add card item for FeedDetailsScreen.kt

--- app/src/main/java/sahraei/hamidreza/pheedly/feature/feeddetails/ui/FeedDetailsScreen.kt
+++ app/src/main/java/sahraei/hamidreza/pheedly/feature/feeddetails/ui/FeedDetailsScreen.kt
@@ -1,9 +1,12 @@
 package sahraei.hamidreza.pheedly.feature.feeddetails.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -11,7 +14,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import sahraei.hamidreza.pheedly.common.CardItem
 import sahraei.hamidreza.pheedly.common.PheedlyProgressItem
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
 
 @Composable
 fun FeedDetailsScreen(
@@ -23,13 +29,24 @@ fun FeedDetailsScreen(
             PheedlyProgressItem()
         }
         state.articles != null -> {
-            LazyColumn {
-                state.articles.forEach {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentPadding = PaddingValues(16.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                items(
+                    items = state.articles,
+                    key = { it }
+                ) {
                     it.title?.let { title ->
-                        item {
-                            Text(text = title)
+                        CardItem(
+                            title = title
+                        ) {
+                            println(" Clicked! ")
                         }
                     }
+
                 }
             }
         }
