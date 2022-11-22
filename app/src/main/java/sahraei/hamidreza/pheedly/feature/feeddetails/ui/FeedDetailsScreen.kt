166b0961fd57a1061fa9b038e963a9f6e423c838
Hamidreza <hamidrezasahraei@cafebazaar.ir>
2022-11-22 15:37:48 +0330

    Remove unused imports from FeedDetailsScreen.kt

--- app/src/main/java/sahraei/hamidreza/pheedly/feature/feeddetails/ui/FeedDetailsScreen.kt
+++ app/src/main/java/sahraei/hamidreza/pheedly/feature/feeddetails/ui/FeedDetailsScreen.kt
@@ -1,23 +1,16 @@
 package sahraei.hamidreza.pheedly.feature.feeddetails.ui
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import sahraei.hamidreza.pheedly.common.CardItem
 import sahraei.hamidreza.pheedly.common.PheedlyProgressItem
-import java.net.URLEncoder
-import java.nio.charset.StandardCharsets
 
 @Composable
 fun FeedDetailsScreen(
