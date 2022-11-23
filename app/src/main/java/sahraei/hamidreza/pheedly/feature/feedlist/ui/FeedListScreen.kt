24d6926a6857027590be01689b79c4b05b982192
Hamidreza <hamidrezasahraei@cafebazaar.ir>
2022-11-23 14:06:46 +0330

    Add preview for FeedListScreen.kt

--- app/src/main/java/sahraei/hamidreza/pheedly/feature/feedlist/ui/FeedListScreen.kt
+++ app/src/main/java/sahraei/hamidreza/pheedly/feature/feedlist/ui/FeedListScreen.kt
@@ -5,23 +5,18 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import sahraei.hamidreza.pheedly.common.compose.CardItem
@@ -105,4 +100,19 @@ fun FloatingActionButton(
     ) {
         Icon(Icons.Filled.Add, "")
     }
+}
+
+@Preview
+@Composable
+fun FeedListPreview(){
+    FeedListSection(
+        feeds = listOf(
+            FeedItem(
+                title = "Sample Title",
+                subtitle = "This is a sample Subtitle",
+                link = "https://google.com"
+            )
+        ),
+        onFeedClicked = { println("On Feed Clicked!") }
+    )
 }
